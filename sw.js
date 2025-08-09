50e1b7d38f64e88b981abe5a1fca1624761427dd
azou3 <azo.uazo08@gmail.com>
2025-08-09 12:20:55 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,75 +1,22 @@
-// This is the service worker file.
-
-const CACHE_NAME = 'prayer-times-cache-v2'; // Updated cache name
-const urlsToCache = [
-  './', // This caches the index.html file at the root.
-  './index.html' // Explicitly cache index.html
-];
-
-// Install event: opens a cache and adds the core files to it.
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
   self.skipWaiting();
 });
 
-// Activate event: cleans up old caches.
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            console.log('Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
+  clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open('prayer-app-cache').then(cache => {
+      return cache.match(event.request).then(response => {
+        return response || fetch(event.request).then(networkResponse => {
+          if (event.request.url.startsWith(self.location.origin)) {
+            cache.put(event.request, networkResponse.clone());
           }
-        })
-      );
+          return networkResponse;
+        });
+      });
     })
   );
-  return self.clients.claim();
-});
-
-// Fetch event: serves cached content when offline.
-self.addEventListener('fetch', event => {
-  // We only want to cache GET requests.
-  if (event.request.method !== 'GET') {
-    return;
-  }
-
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        // Not in cache - fetch from network
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-// Listen for notification clicks
-self.addEventListener('notificationclick', event => {
-    event.notification.close();
-    event.waitUntil(
-        clients.matchAll({ type: 'window', includeUncontrolled: true }).then(clientList => {
-            if (clientList.length > 0) {
-                let client = clientList[0];
-                for (let i = 0; i < clientList.length; i++) {
-                    if (clientList[i].focused) client = clientList[i];
-                }
-                return client.focus();
-            }
-            return clients.openWindow('/');
-        })
-    );
 });
